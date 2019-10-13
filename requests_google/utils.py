21c5fbf6c78c70905e28a9256d75f645ee493752
yangxuhui <yangxuhui1992@163.com>
2019-10-14 00:17:46 +0800

    Added reference

--- requests_google/utils.py
+++ requests_google/utils.py
@@ -5,6 +5,8 @@ requests_google.utils
 
 import urllib
 
+# Reference: 
+# https://stackoverflow.com/questions/51537063/url-format-for-google-news-rss-feed
 URL_LOCATION = "https://news.google.com/news/rss/headlines/section/geo/{location}"
 
 
