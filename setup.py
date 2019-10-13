0ac62b487982251133496dd211aeeb738e022e54
yangxuhui <yangxuhui1992@163.com>
2019-10-13 23:48:27 +0800

    Updated setup

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='requests-google',
-    version='0.0.0',
+    version='0.0.1',
     description='A simple google related Parsing Package.',
     long_description=long_description,
     long_description_content_type='text/markdown',
